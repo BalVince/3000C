e9e35af1eea4315601e98721a7b677b88748e463
Dániel Fodor <fodor.daniel04@gmail.com>
2022-01-25 10:31:56 +0100

    fodor1kesz

--- _1fodor.py
+++ _1fodor.py
@@ -1,6 +1,6 @@
-def van_e_pozitiv(lista):
+def van_e_100zal_oszthato(lista):
     i = 0
-    while i<len(lista) and lista[i] <= 0:
+    while i<len(lista) and lista[i] % 100 != 0:
         i += 1
     if i > lista[i]:
         return False
