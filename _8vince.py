e47a172a18c840e6f27f3dd92287974dec542dc3
Balogh Vince <baloghvince.bv@gmail.com>
2022-01-25 10:27:23 +0100

    Feladatkorrigálás, main.py még nem működik, .py fileok átnevezve

--- _8vince.py
+++ _8vince.py
@@ -1,8 +1,6 @@
 # 8. Van-e a sorozatban olyan negatív szám, amelyet újabb negatív követ?
 
-from main import lista
-
-def nyolc():
+def nyolc(lista):
     for i,e in enumerate(lista[1:]):
         if lista[i-1] < 0 and lista[i] < 0:
             return True
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 # print("Hello world!")
 
-
+import _1fodor, _2fodor, _3fodor, _4csaba, _5csaba, _6csaba, _7csaba, _8vince, _9vince, _10vince
 
 """
-3.000-B
+3.000-C
 Olvassuk be az alábbi fájl tartalmmát egy listába/tömbbe,
 majd a következő feladatokat oldjuk meg.
 Minden feladat előtt a program írja ki a feladat sorszámát!
-1. Van-e a sorozatban pozitív szám?
-2. Hány eleme van a sorozatnak? 
-3. Mennyi a sorozatban található legkisebb szám?
-4. Írjuk ki az első 33-mal osztható szám indexét!
-5. Mennyi a sorozatban található számok átlagának a fele?
-6. Igaz-e, hogy minden szám pozitív?
-7. Hány páratlan szám található a sorozatban?
-8. Van-e a sorozatban olyan negatív szám, amelyet újabb negatív követ?
-9. Írjuk ki az utolsó 19-cel osztható szám indexét!
-10. Írjuk ki a sorozatban található 5-tel osztható számokat!
+1. Van-e a sorozatban 100-zal osztható szám?
+2. Írjuk ki az utolsó 7-tel osztható szám indexét!
+3. Írjuk ki az első 19-cel osztható szám indexét!
+4. Mennyi a sorozatban található számok átlagának a négyzete?
+5. Igaz-e, hogy minden szám nagyobb, mint 10?
+6. Hány 9-cel osztható szám található a sorozatban?
+7. Írjuk ki a sorozatban található 15-tel osztható számok felét!
+8. Hány eleme van a sorozatnak?
+9. Van-e a sorozatban olyan negatív szám, amelyet pozitív követ?
+10. Mennyi a sorozatban található legkisebb szám fele?
 """
 
 lista = []
@@ -54,7 +54,7 @@ print("7. Hány páratlan szám található a sorozatban?\n")
 
 # 8. feladat
 print("8. Van-e a sorozatban olyan negatív szám, amelyet újabb negatív követ?\n")
-
+_8vince.nyolc(lista)
 
 # 9. feladat
 print("9. Írjuk ki az utolsó 19-cel osztható szám indexét!\n")
